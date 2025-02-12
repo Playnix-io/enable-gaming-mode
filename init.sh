1632d2ba3e0e7f845759571458bac9336e87843d
Dragoon Dorise <rodrigosedano@me.com>
2025-02-12 17:53:20 +0100

    fixes

--- init.sh
+++ init.sh
@@ -17,16 +17,16 @@ curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/dow
 #Grub changes
 GRUB_CONFIG="/etc/default/grub"
 GRUB_CMDLINE="amd_pstate=active amd_prefcore=enable iomem=relaxed"
-sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE /" "$GRUB_CONFIG"
+sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE /" "$GRUB_CONFIG"
 sudo update-grub
 
 #Login Fix
-sed -i '/"AutoLoginUser"/a \ \ \ \ \ \ \ \ "CompletedOOBE"\t\t"1"' "$HOME/.steam/registry.vdf"
+#sed -i '/"AutoLoginUser"/a \ \ \ \ \ \ \ \ "CompletedOOBE"\t\t"1"' "$HOME/.steam/registry.vdf"
 
 #boot animation https://github.com/arvigeus/plymouth-theme-steamos
 git clone https://github.com/arvigeus/plymouth-theme-steamos
 mkdir -p /usr/share/plymouth/themes/steamos
-sudo cp -r ./plymouth-theme-steamos/ /usr/share/plymouth/themes/steamos && rm -rf plymouth-theme-steamos
+sudo cp -r ./plymouth-theme-steamos/* /usr/share/plymouth/themes/steamos/ && rm -rf plymouth-theme-steamos
 sudo plymouth-set-default-theme -R steamos
 
 #Clean up
