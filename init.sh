06fd965e565653d0914161f42995724a153c6244
Dragoon Dorise <rodrigosedano@me.com>
2025-05-27 16:44:34 +0200

    arch

--- init.sh
+++ init.sh
@@ -1,12 +1,84 @@
 #!/bin/bash
 
+# Inspired by https://github.com/unlbslk/arch-deckify/
+
+if ! grep -q "^\[multilib\]" /etc/pacman.conf; then
+    echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
+fi
+
 #Dependencies
-sudo pacman -Sy
-sudo pacman -S --noconfirm --needed ryzenadj yay meson base-devel ninja podman libgudev steam
-# No longer needed since February
-#yay -S inputplumber-bin --no-confirm --answerdiff None --answerclean None
-pamac install --no-confirm steam-deckify
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm --needed ryzenadj yay meson base-devel ninja podman mangohud gamescope libgudev steam bluez bluez-utils
+yay -S inputplumber-bin --no-confirm --answerdiff None --answerclean None
+
+yay -S gamescope-session-steam-git --noconfirm --sudoloop
+
+#Auto login
+if [ -f /etc/sddm.conf.d/kde_settings.conf ]; then
+    CONFIG_FILE="/etc/sddm.conf.d/kde_settings.conf"
+else
+    CONFIG_FILE="/usr/lib/sddm/sddm.conf.d/default.conf"
+fi
+sudo sed -i "s/^Relogin=false/Relogin=true/; s/^User=.*/User=$(whoami)/" "$CONFIG_FILE"
+
+echo '#!/usr/bin/bash
+
+if [ -f /etc/sddm.conf.d/kde_settings.conf ]; then
+    CONFIG_FILE="/etc/sddm.conf.d/kde_settings.conf"
+else
+    CONFIG_FILE="/usr/lib/sddm/sddm.conf.d/default.conf"
+fi
+
+# If no arguments are provided, list valid arguments
+if [ $# -eq 0 ]; then
+    echo "Valid arguments: plasma, gamescope"
+    exit 0
+fi
+
+# If the argument is "plasma"
+# IMPORTANT: If you want to use a desktop environment other than KDE Plasma, do not change the IF command.
+# Steam always runs this file as "steamos-session-select plasma" to switch to the desktop.
+# Instead, change the code below that edits the config file.
+
+if [ "$1" == "plasma" ] || [ "$1" == "desktop" ]; then
+
+    echo "Switching session to Desktop."
+    if [ ! -f "$CONFIG_FILE" ]; then
+        echo "SDDM config file could not be found at $CONFIG_FILE."
+        exit 1
+    fi
+    NEW_SESSION='plasma' # For other desktops, change here.
+    sudo sed -i "s/^Session=.*/Session=${NEW_SESSION}/" "$CONFIG_FILE"
+    steam -shutdown
+
+# If the argument is "gamescope"
+elif [ "$1" == "gamescope" ]; then
+
+    echo "Switching session to Gamescope."
+    if [ ! -f "$CONFIG_FILE" ]; then
+        echo "SDDM config file could not be found at $CONFIG_FILE."
+        exit 1
+    fi
+    NEW_SESSION="gamescope-session-steam"
+    sudo sed -i "s/^Session=.*/Session=${NEW_SESSION}/" "$CONFIG_FILE"
+    dbus-send --session --type=method_call --print-reply --dest=org.kde.Shutdown /Shutdown org.kde.Shutdown.logout || gnome-session-quit --logout --no-prompt || cinnamon-session-quit --logout --no-prompt || loginctl terminate-session $XDG_SESSION_ID
+else
+    echo "Valid arguments are: plasma, gamescope."
+    exit 1
+fi' | sudo tee /usr/bin/steamos-session-select > /dev/null
+
+sudo chmod +x /usr/bin/steamos-session-select
+
+sudoers_file="/etc/sudoers.d/sddm_config_edit"
+if [ ! -f "$sudoers_file" ]; then
+    echo "ALL ALL=(ALL) NOPASSWD: /usr/bin/sed -i s/^Session=*/Session=*/ ${CONFIG_FILE}" | sudo tee "$sudoers_file" > /dev/null
+    sudo chmod 440 "$sudoers_file"
+fi
+
+sudo usermod -a -G video $(whoami)
+if ! grep -q 'ACTION=="add", SUBSYSTEM=="backlight"' /etc/udev/rules.d/backlight.rules; then
+    echo 'ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"' | sudo tee -a /etc/udev/rules.d/backlight.rules
+fi
 
 #Launch steam
 /usr/bin/steam > /dev/null 2>&1 &
@@ -15,17 +87,19 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 sudo systemctl enable inputplumber
 sudo systemctl enable inputplumber-suspend
 sudo systemctl start inputplumber
+sudo systemctl enable bluetooth.service
+sudo systemctl start bluetooth.service
 
 #DeckyLoader
 curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sh
 
-#DeckyPlugins
-
-#Grub changes
-GRUB_CONFIG="/etc/default/grub"
-GRUB_CMDLINE="amd_pstate=active amd_prefcore=enable iomem=relaxed"
-sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE /" "$GRUB_CONFIG"
-sudo update-grub
+#Power button suspend
+git clone https://github.com/ShadowBlip/steam-powerbuttond.git
+cd steam-powerbuttond
+chmod +x install.sh
+bash install.sh
+cd ..
+rm -rf steam-powerbuttond
 
 #boot animation https://github.com/arvigeus/plymouth-theme-steamos
 git clone https://github.com/arvigeus/plymouth-theme-steamos
@@ -49,9 +123,6 @@ curl -L -o $registry "https://raw.githubusercontent.com/Playnix-io/enable-gaming
 
 #Back to game mode icon
 curl -L -o "$HOME/Desktop/back.desktop" "https://raw.githubusercontent.com/Playnix-io/enable-gaming-mode/main/back.desktop"
-clear
-sleep 5
-echo "The computer will restart now..."
-exit
-#Rebooting
-#sudo reboot
+cp "$HOME/Desktop/back.desktop" "/usr/share/applications/back.desktop"
+chmod +x "$HOME/Desktop/back.desktop"
+chmod +x "/usr/share/applications/back.desktop"
