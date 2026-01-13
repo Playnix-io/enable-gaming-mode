9801644c94f4f5c1b674369795e139a1002918b5
Dragoon Dorise <rodrigosedano@me.com>
2026-01-13 22:15:16 +0100

    fixes

--- create-pkgs.sh
+++ create-pkgs.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+
 MONTH=$(date +%Y-%m)
-mkdir -p ~/arch-repo/$MONTH/x86_64
-cp /var/cache/pacman/pkg/*.pkg.tar.zst ~/arch-repo/$MONTH/x86_64/
-cd ~/arch-repo/$MONTH/x86_64
-repo-add --sign playnix.db.tar.gz *.pkg.tar.zst
+mkdir -p "$HOME/arch-repo/$MONTH/x86_64"
+cp /var/cache/pacman/pkg/*.pkg.tar.zst "$HOME/arch-repo/$MONTH/x86_64/"
+cd "$HOME/arch-repo/$MONTH/x86_64" || return
+repo-add playnix.db.tar.gz ./*.pkg.tar.zst
 
 
 STAGING_REPO=~/arch-repo/$MONTH/
 WEB_USER=almalinux            # el usuario SSH en el webserver
 WEB_HOST=os.playnix.io
-WEB_REPO=/var/www/html/vhosts/os.playnix.io/arch-repo/x86_64/$MONTH/
+WEB_REPO=/var/www/vhosts/os.playnix.io/html/arch-repo/$MONTH/x86_64/
 
 # 1) Copia paquetes y DB
 rsync -av --delete "$STAGING_REPO" "$WEB_USER@$WEB_HOST:$WEB_REPO"
