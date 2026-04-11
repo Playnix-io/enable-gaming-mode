74221c265683228ad107b8c35c168fa6ae444310
Rodrigo <rodrigosedano@me.com>
2026-04-11 23:35:45 +0200

    Revert "1.3b"
    This reverts commit ec20feff87cf7745f338e1b3aa5b1d935585d95c.

--- remote-boot-custom-actions.sh
+++ remote-boot-custom-actions.sh
@@ -302,7 +302,7 @@ if [ "$ENABLED_UPDATE" = true ]; then
                 if [[ "$VERSION_ID_CURRENT" < "1.3" ]]; then        
                   fix_sonic_mania            
                   #fix_branch_message
-                  add_sd_automount
+                  #add_sd_automount
                 fi     
             fi 
 
