508b6cbfa1b7f6507d2d42a625244c12492cdf61
Rodrigo <rodrigosedano@me.com>
2026-04-11 23:32:33 +0200

    1.3a

--- remote-boot-custom-actions.sh
+++ remote-boot-custom-actions.sh
@@ -301,8 +301,8 @@ if [ "$ENABLED_UPDATE" = true ]; then
                 fi  
                 if [[ "$VERSION_ID_CURRENT" < "1.3" ]]; then        
                   fix_sonic_mania            
-                  fix_branch_message
-                  add_sd_automount
+                  #fix_branch_message
+                  #add_sd_automount
                 fi     
             fi 
 
